a7e5a574339ff9cdff98be7cb1fa55ae4d46cee8
eshl12066 <2986512084@qq.com>
2019-10-18 00:11:08 +0800

    git send local repository

---
+++ gitssm/src/com/shl/git/Demo1.java
@@ -0,0 +1,11 @@
+package com.shl.git;
+
+/**
+ * @author 狗蛋
+ * @create 2019-10-18-0:03
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("git idea test");
+    }
+}
